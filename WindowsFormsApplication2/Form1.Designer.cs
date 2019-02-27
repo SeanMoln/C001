64dee2e561d634cf77f475450e6be60e78ac9220
Shute <Shute@>
2019-02-27 11:09:00 +0800

    first

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.Up = new System.Windows.Forms.Button();
+            this.Down = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(93, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(158, 151);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // Up
+            // 
+            this.Up.Enabled = false;
+            this.Up.Location = new System.Drawing.Point(12, 12);
+            this.Up.Name = "Up";
+            this.Up.Size = new System.Drawing.Size(75, 23);
+            this.Up.TabIndex = 1;
+            this.Up.Text = "↑";
+            this.Up.UseVisualStyleBackColor = true;
+            this.Up.Click += new System.EventHandler(this.Up_Click);
+            // 
+            // Down
+            // 
+            this.Down.Enabled = false;
+            this.Down.Location = new System.Drawing.Point(12, 52);
+            this.Down.Name = "Down";
+            this.Down.Size = new System.Drawing.Size(75, 23);
+            this.Down.TabIndex = 2;
+            this.Down.Text = "↓";
+            this.Down.UseVisualStyleBackColor = true;
+            this.Down.Click += new System.EventHandler(this.Down_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(12, 140);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 169);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(239, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "載入XML文件";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(12, 198);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(239, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "匯出XML文件";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "XML檔案(*.xml) | *.xml";
+            this.openFileDialog1.Title = "開起舊檔";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 232);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Down);
+            this.Controls.Add(this.Up);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button Up;
+        private System.Windows.Forms.Button Down;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;//LINQ TO XML
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        XElement xe;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var xenew = XElement.Load(openFileDialog1.FileName);
+                foreach (XElement xn in xenew.Elements("book"))
+                {
+                    var xo = from x in xe.Elements("book")
+                             where x.Element("book").Value.Equals(xn.Element("book_id").Value)
+                             select x;
+                    if (xo.Count() > 0)
+                    {
+                        if (MessageBox.Show(xn.Element("book_id").Value + "重複，確認覆蓋嗎?", "XML瀏覽器",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                        {
+                            xo.First().ReplaceWith(xn);
+                        }
+                    }
+                    else
+                    {
+                        xe.Add(xn);
+
+
+
+                    }
+                }
+                treeView1.BeginUpdate();
+                treeView1.Nodes.Clear();
+                var trn = new TreeNode(xe.Name.ToString());
+                treeView1.Nodes.Add(trn);
+                AddNode(xe.Elements(), trn);
+                treeView1.EndUpdate();
+                treeView1.ExpandAll();
+            }
+        }
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //開檔案對話窗
+            {
+                treeView1.BeginUpdate();
+                treeView1.Nodes.Clear();
+                xe = XElement.Load(openFileDialog1.FileName);
+                var trn = new TreeNode(xe.Name.ToString());
+                treeView1.Nodes.Add(trn);
+                AddNode(xe.Elements(), trn);
+                treeView1.EndUpdate();
+                treeView1.ExpandAll();
+
+                Down.Enabled = true;
+                Up.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button6.Enabled = true;
+
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        private void AddNode(IEnumerable<XElement> xes, TreeNode treeNode)
+        {
+            foreach (XElement xn in xes)
+            {
+                var tn = new TreeNode(xn.Name.ToString());
+                treeNode.Nodes.Add(tn);
+
+                if (xn.Elements().Count() > 0)
+                {
+                    AddNode(xn.Elements(), tn);
+
+                }
+                else
+                {
+                    tn.Nodes.Add(xn.Value.ToString());
+                }
+
+            }
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void Up_Click(object sender, EventArgs e)
+        {
+            var tn = treeView1.SelectedNode;
+            if(tn != null && tn.Parent != null)
+            {
+                while (tn.Text != "book")
+                {
+                    tn = tn.Parent;
+
+                }
+                var pn = tn.PrevNode;
+                if(pn != null)
+                {
+                    treeView1.BeginUpdate();
+                    tn.Parent.Nodes.Remove(pn);
+                    tn.Parent.Nodes.Insert(tn.Parent.Nodes.IndexOf(tn) + 1, pn);
+                    treeView1.EndUpdate();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Down_Click(object sender, EventArgs e)
+        {
+            var tn = treeView1.SelectedNode;
+            if (tn != null && tn.Parent != null)
+            {
+                while (tn.Text != "book")
+                {
+                    tn = tn.Parent;
+
+                }
+                var pn = tn.NextNode;
+                if (pn != null)
+                {
+                    treeView1.BeginUpdate();
+                    tn.Parent.Nodes.Remove(pn);
+                    tn.Parent.Nodes.Insert(tn.Parent.Nodes.IndexOf(tn) - 1, pn);
+                    treeView1.EndUpdate();
+                }
+            }
+        }
+    }
+}
